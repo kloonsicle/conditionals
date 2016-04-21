dafa81e47d5dcbadbd6d21a54ac2890e72898766
zahras09 <victoriayiyau@yahoo.com>
2016-04-20 21:13:10 -0700

    in class conditional exercises

---
+++ conditionals.py
@@ -0,0 +1,59 @@
+#Activity 2 
+#April 20. 2016
+
+
+
+#Which name is greater?
+
+# name1 = raw_input("What is your name?")
+# name2 = raw_input("What is your pair's name?")
+
+# if name1 > name2 :
+# 	print "My name is greater!"
+# elif name2 > name1 :
+# 	print "Their name is greater."
+# else:
+# 	print "Our names are equal!"
+ 
+
+
+#Is it half way thru the month?
+
+# date = int(raw_input("What is the day of the month?"))
+
+# if date <=15 :
+# 	print "The month is still young."
+# elif date >15 and date <=31:
+# 	print "Oh, we're halfway there."
+# else:
+# 	print "Oops thats not a date. Please try again."
+
+
+
+#Day of the week.
+
+# today = raw_input("What is the day of the week?")
+
+# if today == "Monday":
+# 	print "Yay, class day!"
+# elif today == "Tuesday":
+# 	print "Sigh, its only Tuesday."
+# elif today == "Wednesday":
+# 	print "Humpday!"
+# elif today == "Thursday":
+# 	print "#tbt"
+# elif today == "Friday":
+# 	print "TGIF!"
+# else:
+# 	print "Yay, it's the weekend!"
+
+
+#Is it even
+
+number = int(raw_input("What is the number?"))
+
+if number % 2 == 0:
+	print "The number %i is even." % (number)
+else:
+	print "The number %i is odd." % (number)
+
